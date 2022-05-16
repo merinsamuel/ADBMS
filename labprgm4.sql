8140f994fe5b8436c9fc5e089bb65eccaef7115f
merinsamuel <103182657+merinsamuel@users.noreply.github.com>
2022-05-15 21:17:03 -0700

    Add files via upload

---
+++ labprgm4.sql
@@ -0,0 +1,5 @@
+select pname,price from product where pname!='soap';
+select min(price),max(price),avg(price) from product;
+select city,count(city) as count from customer group by city;
+select city,count(city) as count from customer group by city having count(city)>3;
+select concat(name,' lives in ',city) as 'who,where' from customer;
