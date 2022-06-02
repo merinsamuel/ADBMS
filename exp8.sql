ecd49d5effcda9fbadebd11ff8563d14cd2dac9e
merinsamuel <103182657+merinsamuel@users.noreply.github.com>
2022-06-02 02:33:17 -0700

    Add files via upload

---
+++ exp8.sql
@@ -0,0 +1,25 @@
+use mcetmca2021;
+select * from customer;
+delimiter 
+create procedure  list_names(inout name_list varchar(4000))
+begin
+declare is_done integer default 0;
+declare c_name varchar(100)default"";
+declare cust_cursor cursor for
+select name from customer;
+declare continue handler for not found set is_done=1;
+open cust_cursor;
+get_list:loop
+fetch cust_cursor into c_name;
+if is_done=1 then
+leave get_list;
+end if;
+set name_list=concate(c_name,";",name_list);
+end loop get_list;
+close cust_cursor;
+end$$
+delimiter ;
+set @name_list="";
+call list_names(@name_list);
+SELECT @name_list;
+                                                                                                                                                                                                                                                                                                                                                                          
